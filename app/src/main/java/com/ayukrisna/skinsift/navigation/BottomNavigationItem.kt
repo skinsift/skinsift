31ee17afd73e0f5e52add7af34aa22f3349a6560
ayuukrisn <ayuukrisn@gmail.com>
2024-12-01 12:09:06 +0800

    done fixing navbar

--- app/src/main/java/com/ayukrisna/skinsift/navigation/BottomNavigationItem.kt
+++ app/src/main/java/com/ayukrisna/skinsift/navigation/BottomNavigationItem.kt
@@ -1,15 +1,20 @@
 package com.ayukrisna.skinsift.navigation
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -24,13 +29,15 @@ data class TopLevelRoute<T : Any>(
 )
 
 @Composable
-fun BottomNavigationBar(navController: NavController) {
+fun BottomNavigationBar(
+    navController: NavController
+) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
     BottomNavigation (
-        backgroundColor = MaterialTheme.colorScheme.surface,
-        contentColor = MaterialTheme.colorScheme.onSurface
+        backgroundColor = Color(0xFFFFFFFF),
+        contentColor = MaterialTheme.colorScheme.primary
     ){
         val topLevelRoutes = listOf(
             TopLevelRoute(
@@ -59,18 +66,30 @@ fun BottomNavigationBar(navController: NavController) {
                 inactiveIcon = R.drawable.outline_profile
             )
         )
-
-        topLevelRoutes.forEach{ topLevelRoute ->
-            val isSelected = currentDestination?.route == topLevelRoute.route
+        var selectedItemIndex by rememberSaveable {
+            mutableStateOf(0)
+        }
+        topLevelRoutes.forEachIndexed{ index, topLevelRoute ->
             BottomNavigationItem(
-                selected = currentDestination?.hierarchy?.any { it.route == topLevelRoute.route } == true,
-                icon = { painterResource(id =
-                if (isSelected) {
-                    topLevelRoute.activeIcon
-                } else {
-                    topLevelRoute.inactiveIcon
-                }) },
+                selected = selectedItemIndex == index,
+                icon = {
+                    Icon(
+                        painter = painterResource(
+                            id = if (index == selectedItemIndex) {
+                                topLevelRoute.activeIcon
+                            } else {
+                                topLevelRoute.inactiveIcon
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                },
+//                label = {
+//                    Text(text = topLevelRoute.title)
+//                },
                 onClick = {
+                    selectedItemIndex = index
                     navController.navigate(topLevelRoute.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
                             saveState = true
